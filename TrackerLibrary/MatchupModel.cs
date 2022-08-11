78cb2ee0aa167300d2df329c12e3d21b4535f19c
Korie Promise <eracle.co@gmail.com>
2022-08-11 23:13:48 +0100

    Created the class library, added all the models and their properties

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public int Id { get; set; }
+        public List<MatchupEntryModel> Entries { get; set; }
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
